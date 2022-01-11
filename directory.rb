987b5dd1b15b51c1edf9b28c350945e90f6f1782
Shirley Macfarlane <shirley.macfarlane@waitrose.co.uk>
2022-01-11 12:25:02 +0000

    Refractor to ask user for input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,18 @@
-# let's put all students into an array
-students = [
-    {name: "Dr. Hannibal Lector", cohort: :november},
-    {name:"Darth Vader", cohort: :november},
-    {name:"Nurse Ratched", cohort: :november},
-    {name:"Michael Corleone", cohort: :november},
-    {name:"Alex DeLarge", cohort: :november},
-    {name:"The Wicked Witch of the West", cohort: :november},
-    {name:"Terminator", cohort: :november},
-    {name:"Freddy Krueger", cohort: :november},
-    {name:"The Joker", cohort: :november},
-    {name:"Joffrey Baratheon", cohort: :november},
-    {name:"Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twicwe"
+  #create an empty array"
+  students = []
+  name = gets.chomp 
+  #While the first name is not empty, repeat this code
+  while !name.empty? do
+    #add the students name to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end 
+  students
+end
 
 def print_header
   puts "The students of Villians Academy"
@@ -28,7 +29,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-#nothing happend until we call the module
+#nothing will happenuntil we call the module
+students = input_students
 print_header
 print(students)
 print_footer(students)
